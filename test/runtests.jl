f20bb5bbe5d2c459844b9a184a09dce8f96f4d88
jezekon <78500480+jezekon@users.noreply.github.com>
2025-03-17 23:13:47 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,8 @@ using Implicit2TetMesh
 
   taskName = "beam_vfrac_04_B-1.0_smooth-1_Approximation"
 
-  @load "Z_cantilever_beam_vfrac_04_FineGrid_B-1.0_smooth-1_Approximation.jld2" fine_grid
-  @load "Z_cantilever_beam_vfrac_04_FineSDF_B-1.0_smooth-1_Approximation.jld2" fine_sdf
+  @load "../data/Z_cantilever_beam_vfrac_04_FineGrid_B-1.0_smooth-1_Approximation.jld2" fine_grid
+  @load "../data/Z_cantilever_beam_vfrac_04_FineSDF_B-1.0_smooth-1_Approximation.jld2" fine_sdf
 
   scheme = "A15"
   
@@ -27,25 +27,25 @@ using Implicit2TetMesh
   # assess_mesh_quality(mesh, "initial_mesh")
   warp!(mesh) # Warp nearest nodes to the isocontour
 
-  update_connectivity!(mesh) # Update mesh topology
-
-  slice_ambiguous_tetrahedra!(mesh) # Remove elements outside the body
-
-  update_connectivity!(mesh)
-
-  adjust_nodes_to_isosurface!(mesh) # Simple cut of elements to follow the isocontour
-
-  TetMesh_volumes(mesh)
-  optimize_mesh!(mesh)
-
-  export_mesh_vtk(mesh, "$(name)_TriMesh.vtu")
-
-  # Apply cutting planes only if they are defined
-  if !isempty(plane_definitions)
-    warp_mesh_by_planes_sdf!(mesh, plane_definitions, warp_param)
-    update_connectivity!(mesh)
-    export_mesh_vtk(mesh, "$(name)_TriMesh_cut.vtu")
-  end
-
-  TetMesh_volumes(mesh)
+  # update_connectivity!(mesh) # Update mesh topology
+  #
+  # slice_ambiguous_tetrahedra!(mesh) # Remove elements outside the body
+  #
+  # update_connectivity!(mesh)
+  #
+  # adjust_nodes_to_isosurface!(mesh) # Simple cut of elements to follow the isocontour
+  #
+  # TetMesh_volumes(mesh)
+  # optimize_mesh!(mesh)
+  #
+  # export_mesh_vtk(mesh, "$(name)_TriMesh.vtu")
+  #
+  # # Apply cutting planes only if they are defined
+  # if !isempty(plane_definitions)
+  #   warp_mesh_by_planes_sdf!(mesh, plane_definitions, warp_param)
+  #   update_connectivity!(mesh)
+  #   export_mesh_vtk(mesh, "$(name)_TriMesh_cut.vtu")
+  # end
+  #
+  # TetMesh_volumes(mesh)
 end
