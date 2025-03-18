70c5e99e7538136d377702e4e5a1f58dd17df677
jezekon <78500480+jezekon@users.noreply.github.com>
2025-03-18 16:46:40 +0100

    Update GenerateMesh.jl

--- src/GenerateMesh/GenerateMesh.jl
+++ src/GenerateMesh/GenerateMesh.jl
@@ -1,11 +1,13 @@
 module GenerateMesh
 
-export generate_mesh!, warp!, update_connectivity!, slice_ambiguous_tetrahedra!, adjust_nodes_to_isosurface!, export_mesh_vtk, warp_node_to_isocontour!, longest_edge
-
+export generate_mesh!, warp!, update_connectivity!, slice_ambiguous_tetrahedra!, adjust_nodes_to_isosurface!, export_mesh_vtk, warp_node_to_isocontour!, longest_edge, remove_nodes_outside_isocontour!, remove_inverted_elements!
+ 
+  
 using Statistics
 using StaticArrays
 using LinearAlgebra
 using WriteVTK
+using Printf 
 
 using Implicit2TetMesh.Fundamentals
 
