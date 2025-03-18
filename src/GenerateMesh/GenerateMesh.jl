aedd1a596a53560d7d50034f124374c6b9a818a0
jezekon <78500480+jezekon@users.noreply.github.com>
2025-03-18 11:05:21 +0100

    Update GenerateMesh.jl
    add export warp_node_to_isocontour!, longest_edge

--- src/GenerateMesh/GenerateMesh.jl
+++ src/GenerateMesh/GenerateMesh.jl
@@ -1,6 +1,6 @@
 module GenerateMesh
 
-export generate_mesh!, warp!, update_connectivity!, slice_ambiguous_tetrahedra!, adjust_nodes_to_isosurface!, export_mesh_vtk
+export generate_mesh!, warp!, update_connectivity!, slice_ambiguous_tetrahedra!, adjust_nodes_to_isosurface!, export_mesh_vtk, warp_node_to_isocontour!, longest_edge
 
 using Statistics
 using StaticArrays
