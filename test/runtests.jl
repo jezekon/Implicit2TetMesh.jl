6a2375fe15c6a45ec689e83c5682cdda7ce4d6fa
jezekon <78500480+jezekon@users.noreply.github.com>
2025-05-15 21:39:08 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,21 +10,21 @@ using Implicit2TetMesh.Utils
 
 @testset "Implicit2TetMesh.jl" begin
 
-  RUN_beam = false
+  RUN_beam = true
   RUN_main = false
-  RUN_main_param = true
+  RUN_main_param = false
 
   if RUN_beam
 
-      taskName = "beam_vfrac_04_B-1.0_smooth-1_Approx"
-      # taskName = "beam_vfrac_04_B-1.0_smooth-1_Interp"
+      # taskName = "beam_vfrac_04_B-1.0_smooth-1_Approx"
+      taskName = "beam_vfrac_04_B-1.0_smooth-1_Interp"
       # taskName = "beam_vfrac_04_B-1.0_smooth-2_Approx"
       # taskName = "beam_vfrac_04_B-1.0_smooth-2_Interp"
 
-      @load "../data/Z_cantilever_beam_vfrac_04_FineGrid_B-1.0_smooth-1_Approximation.jld2" fine_grid
-      @load "../data/Z_cantilever_beam_vfrac_04_FineSDF_B-1.0_smooth-1_Approximation.jld2" fine_sdf
-      # @load "../data/Z_cantilever_beam_vfrac_04_FineGrid_B-1.0_smooth-1_Interpolation.jld2" fine_grid
-      # @load "../data/Z_cantilever_beam_vfrac_04_FineSDF_B-1.0_smooth-1_Interpolation.jld2" fine_sdf
+      # @load "../data/Z_cantilever_beam_vfrac_04_FineGrid_B-1.0_smooth-1_Approximation.jld2" fine_grid
+      # @load "../data/Z_cantilever_beam_vfrac_04_FineSDF_B-1.0_smooth-1_Approximation.jld2" fine_sdf
+      @load "../data/Z_cantilever_beam_vfrac_04_FineGrid_B-1.0_smooth-1_Interpolation.jld2" fine_grid
+      @load "../data/Z_cantilever_beam_vfrac_04_FineSDF_B-1.0_smooth-1_Interpolation.jld2" fine_sdf
       # @load "../data/Z_cantilever_beam_vfrac_04_FineGrid_B-1.0_smooth-2_Approximation.jld2" fine_grid
       # @load "../data/Z_cantilever_beam_vfrac_04_FineSDF_B-1.0_smooth-2_Approximation.jld2" fine_sdf
       # @load "../data/Z_cantilever_beam_vfrac_04_FineGrid_B-1.0_smooth-2_Interpolation.jld2" fine_grid
@@ -34,10 +34,10 @@ using Implicit2TetMesh.Utils
       # scheme = "Schlafli"
       
       # Definice rovin
-      plane_definitions = [
-        PlaneDefinition([-1.0, 0.0, 0.0], [0.0, 10.0, 0.0], Square(30.)),
-        PlaneDefinition([1.0, 0.0, 0.0], [60.0, 2.0, 2.0], Square(5.))
-      ]
+      # plane_definitions = [
+      #   PlaneDefinition([-1.0, 0.0, 0.0], [0.0, 10.0, 0.0], Square(30.)),
+      #   PlaneDefinition([1.0, 0.0, 0.0], [60.0, 2.0, 2.0], Square(5.))
+      # ]
       warp_param = 0.3
 
       mesh = BlockMesh(fine_sdf, fine_grid)
@@ -45,40 +45,44 @@ using Implicit2TetMesh.Utils
       # Choose scheme: "A15" or "Schlafli"
       generate_mesh!(mesh, scheme)
 
-      # assess_mesh_quality(mesh, "initial_mesh")
-      warp!(mesh) # Warp nearest nodes to the isocontour
+      warp!(mesh, scheme) # Warp nearest nodes to the isocontour
+      count_negative_determinants(mesh)
+      export_mesh_vtu_quality(mesh, "$(taskName)_TriMesh-step_warp$(scheme).vtu")
 
-      update_connectivity!(mesh) # Update mesh topology
 
-      slice_ambiguous_tetrahedra!(mesh) # Remove elements outside the body
-
-      update_connectivity!(mesh)
-
-      export_mesh_vtu(mesh, "$(taskName)_TriMesh-notOPT_$(scheme).vtu")
-
-      # slice_mesh_with_plane!(mesh, "x", 0.6, export_file="sliced_mesh_notOPT.vtu")
-
-      # adjust_nodes_to_isosurface!(mesh) # Simple cut of elements to follow the isocontour
+      # update_connectivity!(mesh) # Update mesh topology
       #
-      TetMesh_volumes(mesh)
-      optimize_mesh!(mesh)
+      # slice_ambiguous_tetrahedra!(mesh) # Remove elements outside the body
       #
-      remove_inverted_elements!(mesh)
-      export_mesh_vtu(mesh, "$(taskName)_TriMesh-$(scheme).vtu")
-      export_mesh_vtu_quality(mesh, "$(taskName)_TriMesh-$(scheme)_quality.vtu")
-
-       # Apply cutting planes only if they are defined
-       if !isempty(plane_definitions)
-         warp_mesh_by_planes_sdf!(mesh, plane_definitions, warp_param)
-         update_connectivity!(mesh)
-         export_mesh_vtu(mesh, "$(taskName)_TriMesh-$(scheme)_cut.vtu")# -> kladný jacob
-       end
-
-      TetMesh_volumes(mesh)
-
-      export_mesh_vtu(mesh, "$(taskName)_TriMesh-$(scheme)_plane.vtu")
-      # slice_mesh_with_plane!(mesh, "x", 0.6, export_file="sliced_mesh_OPT.vtu")
-      # assess_mesh_quality(mesh, "mesh_quality")
+      # update_connectivity!(mesh)
+      #
+      # export_mesh_vtu_quality(mesh, "$(taskName)_TriMesh-notOPT_$(scheme).vtu")
+      #
+      # # slice_mesh_with_plane!(mesh, "x", 0.6, export_file="sliced_mesh_notOPT.vtu")
+      #
+      # # adjust_nodes_to_isosurface!(mesh) # Simple cut of elements to follow the isocontour
+      # #
+      # TetMesh_volumes(mesh)
+      # remove_inverted_elements!(mesh)
+      # optimize_mesh!(mesh, scheme)
+      # #
+      # remove_inverted_elements!(mesh)
+      # export_mesh_vtu(mesh, "$(taskName)_TriMesh-$(scheme).vtu")
+      #
+      #  # Apply cutting planes only if they are defined
+      #  # if !isempty(plane_definitions)
+      #  #   warp_mesh_by_planes_sdf!(mesh, plane_definitions, warp_param)
+      #  #   update_connectivity!(mesh)
+      #  #   export_mesh_vtu(mesh, "$(taskName)_TriMesh-$(scheme)_cut.vtu")
+      #  # end
+      #
+      # TetMesh_volumes(mesh)
+      #
+      # export_mesh_vtu(mesh, "$(taskName)_TriMesh-$(scheme)_plane.vtu")
+      # slice_mesh_with_plane!(mesh, "x", 0.6)
+      # # update_connectivity!(mesh)
+      # export_mesh_vtu_quality(mesh, "$(taskName)_TriMesh-$(scheme)_quality.vtu")
+      # # assess_mesh_quality(mesh, "mesh_quality")
   end
 
   if RUN_main
